419bfc362e3970961fdf556b0a8cbb29138a3a34
ayeshamumtaz88 <mumtazayesha863@gmail.com>
2026-02-09 23:59:15 -0800

    Added DSA LAB folder with week 1 and week 2

---
+++ DSA LAB/DSA Lab week 1/ayesha task 2.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter " << n << " elements:" << endl;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    cout << "Original Array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for(int i = 0; i < n / 2; i++) {
+        int temp = arr[i];
+        arr[i] = arr[n - i - 1];
+        arr[n - i - 1] = temp;
+    }
+    cout << "Reversed Array: ";
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
